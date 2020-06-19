4812efed91438ed0b2d2ecf71a391ac7b60d8d1e
drico <drico@DELL-5437>
2020-06-18 23:54:01 -0300

    1- Criar uma estrutura de dados não-homogênea para exemplificação dos dados gerados no cenário escolhido pelo grupo. (classe peça)

---
+++ src/model/Peca.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author drico
+ */
+public class Peca {
+    
+    private int id;
+    private double preco;
+    private String marca;
+    private String nome;
+    private String descricao;
+
+    public Peca() {
+    }
+
+    public Peca(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Peca{" + "id=" + id + ", preco=" + preco + ", nome=" + nome + '}';
+    }
+    
+        
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
