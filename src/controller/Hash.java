7d67e9196668ccf071d8849cba25ec43818bdb3e
drico <drico@DELL-5437>
2020-07-01 02:04:35 -0300

    comentarios classe hash

--- src/controller/Hash.java
+++ src/controller/Hash.java
@@ -10,7 +10,7 @@ public class Hash {
     private int idsLength;
     private Lista[] elementos;
     private long tempoBusca;
-    private int idAtual;
+    private long idAtual;
 
     public Hash(int numeroElementos) {
 
@@ -20,82 +20,102 @@ public class Hash {
         // vetor para busca direta por id
         idsLength = numeroElementos + (numeroElementos / 4);
         ids = new Peca[idsLength];
-        
+
         idAtual = 1;
     }
 
     // insere um elemento na haspmap
     public void put(Peca elemento) {
 
-        while(elemento.getId() >= idsLength) {
+        // verifica o tamanho do vetor de ids
+        while (elemento.getId() >= idsLength) {
 
             capacity();
 
         }
 
+        // Faz referencia ao elemento na posição do vetor correspondente a id
         ids[elemento.getId()] = elemento;
+        //calcula o valor da chave
         key = callKell(elemento.getNome());
-
+        // verifica a posição caso não tenha elemento cria um novo e insere na posição
         if (elementos[key] == null) {
             elementos[key] = new Lista();
         }
+        // caso tenha elemento a responsabilidade de tratar é da classe lista
         elementos[key].insert(elemento);
         idAtual++;
     }
 
+    // Metodo de busca por nome
     public Peca search(String nome) {
 
         long inicio = System.currentTimeMillis();
 
+        // calcula a chave usando o nome
         key = callKell(nome);
 
         if (elementos[key] == null) {
-            return null;
-        }
-
-        Peca busca = elementos[key].search(nome);
-
-        if (busca == null) {
             long fim = System.currentTimeMillis();
             tempoBusca = (int) (fim - inicio);
             return null;
         }
+        
+        // cria um novo que recebe o elemento na posição da chave chama no metodo de busca para percorrer os elementod da posição
+        Peca busca = elementos[key].search(nome);
 
+        if (busca == null) {
+            long fim = System.currentTimeMillis();
+            tempoBusca = (fim - inicio);
+            return null;
+        }
+
+        // retorna o elemendo 
         long fim = System.currentTimeMillis();
         tempoBusca = fim - inicio;
         return busca;
 
     }
 
+    //metodo de busca por id
     public Peca search(int id) {
-
+        
+        // retorna o elemento da posição correspondente
         return ids[id];
 
     }
 
-    public void remove(Peca peca){
-        
+    // metodo para remover elemento
+    public void remove(Peca peca) {
+
+        // remove elemento da posição correspondente
         ids[peca.getId()] = null;
+        //calcula a chave
         key = callKell(peca.getNome());
+        //a responsabilidade de remover é da classe lista
         elementos[key].remove(peca.getId());
         numeroElementos--;
-        
+
     }
-    
+
     // calcula a chave usando o nome da peça
     private int callKell(String nome) {
 
+        //cria e coloca a String em um vetor de byte
         byte[] code = nome.getBytes();
         int soma = 0;
 
+        //soma os valores da tabela ascii correspondente de cada letra do nome
         for (int i = 0; i < code.length; i++) {
             soma += code[i];
         }
-
+        
+        // retorna o resto da divisão com o numero de elementos 
         return soma % numeroElementos;
 
     }
 
+    
     private void capacity() {
 
         int novoIdsLength = idsLength + (idsLength / 2);
@@ -120,9 +140,9 @@ public class Hash {
     public void setNumeroElementos(int numeroElementos) {
         this.numeroElementos = numeroElementos;
     }
-    
-    public Peca[] getPecas(){
-        
+
+    public Peca[] getPecas() {
+
         return ids;
     }
 
@@ -134,10 +154,8 @@ public class Hash {
         return tempoBusca;
     }
 
-    public int getIdAtual() {
+    public long getIdAtual() {
         return idAtual;
     }
-    
-    
 
 }
